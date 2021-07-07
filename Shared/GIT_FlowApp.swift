07d8e2507f4f694386e21eb198ba57d1c6bc554b
Ani Poletich <poletichanna@mail.ru>
2021-07-07 23:45:07 +0300

    Initial Commit

---
+++ Shared/GIT_FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIT_FlowApp.swift
+//  Shared
+//
+//  Created by anna on 07.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct GIT_FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
